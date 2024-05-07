5420cc48f396377004bcbb35fec26de0220a3311
adrianrangel07 <adrianrangel923@gmail.com>
2024-05-07 18:56:25 -0500

    Footer bien colocado

--- src/main/resources/static/js/Inicio.js
+++ src/main/resources/static/js/Inicio.js
@@ -76,7 +76,22 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const offerContainers = document.querySelectorAll('.offer');
+    const footer = document.querySelector('.footer');
 
+    function checkOfferPresence() {
+        if (offerContainers.length === 0) {
+            footer.style.position = 'fixed';
+        } else {
+            footer.style.position = 'relative';
+        }
+    }
+
+    checkOfferPresence();
+
+    window.addEventListener('resize', checkOfferPresence);
+});
 
 
 
